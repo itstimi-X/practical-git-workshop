834bbae0b5e21569c9378e9d9745ae35eeab2745
김인후 <ssk0710402@gmail.com>
2025-07-01 17:57:36 +0900

    feat:사용자 인증 기능 구현

--- project/app.js
+++ project/app.js
@@ -1,4 +1,5 @@
 // Git Workshop 실습용 앱
+console.log("급하게 추가한 로그인 기능");
 console.log("안녕하세요! Git 실습을 시작합니다.");
 
 function greet(name) {
@@ -6,3 +7,5 @@ function greet(name) {
 }
 
 console.log(greet("Git 사용자"));
+
+console.log("추가한 로그인 기능");
